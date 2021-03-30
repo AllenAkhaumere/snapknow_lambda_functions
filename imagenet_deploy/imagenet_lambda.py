89f5b404d53ae34850a27167ffaf52f4ac2df683
Allen Akhaumere <allenakhaumere@gmail.com>
2021-03-31 00:36:16 +0100

    added resnet18 model

--- imagenet_deploy/imagenet_lambda.py
+++ imagenet_deploy/imagenet_lambda.py
@@ -20,15 +20,21 @@ image_transforms = transforms.Compose([
                          [0.229, 0.224, 0.225])
 ])
 
+#initialized resnet18 model achitecture
 model = models.resnet18()
-model.load_state_dict(torch.load('/home/allen/SnapKnow Deploy/data/resnet18-5c106cde.pth'))
+#load the pretrained weight from directory
+model.load_state_dict(torch.load('/home/allen/Documents/GitHub/snapknow_lambda_functions/assets/resnet18-5c106cde.pth'))
+#Put model on evaluation mode for inference
 model.eval()
 
 
 
 global labels_map
+#load the file from our directory
 json_file = open('/home/allen/SnapKnow Deploy/data/index_to_name.json')
+#read the content of the file
 json_str = json_file.read()
+#load the content in a json
 labels_map = json.loads(json_str)
 
 def transform_image(image): 
@@ -40,7 +46,7 @@ def lambda_handler(url):
     image = Image.open(urllib.request.urlopen(url))
     image = transform_image(image)
     image = image.view(-1, 3, 224, 224)
-    prediction = F.softmax(model(image)[0])
+    prediction = F.softmax(input=model(image)[0], dim=0)
     topk_probs, topk_idxs = torch.topk(prediction, k=3)
     result = []
     for i in range(len(topk_idxs)):
@@ -50,7 +56,14 @@ def lambda_handler(url):
 
     return {'Response': result}
 
-url = 'https://raw.githubusercontent.com/shivajid/Yolo/master/data/eagle.jpg'
-print(lambda_handler(url=url))
+
+url1 = 'https://raw.githubusercontent.com/kimx3314/Stanford-Cars-Dataset-Vehicle-Recognition/master/additional_images/AM%20General%20Hummer%20SUV/AM%20General%20Hummer%20SUV.jpg'
+url2 = 'https://raw.githubusercontent.com/simoninithomas/Dog-Breed-Classifier/master/images/American_water_spaniel_00648.jpg'
+url3 = 'https://raw.githubusercontent.com/simoninithomas/Dog-Breed-Classifier/master/images/Curly-coated_retriever_03896.jpg'
+url4 = 'https://raw.githubusercontent.com/simoninithomas/Dog-Breed-Classifier/master/images/Labrador_retriever_06455.jpg'
+print(lambda_handler(url=url1))
+print(lambda_handler(url=url2))
+print(lambda_handler(url=url3))
+print(lambda_handler(url=url4))
 
 
