54e62ffeab6face3615d8b0fb8a02f25e26d1453
Allen Akhaumere <allenakhaumere@gmail.com>
2021-05-22 14:16:50 +0100

    damage tyre detection  model

---
+++ tyre_deploy/tyre_detect.py
@@ -0,0 +1,40 @@
+import yolov5
+from io import BytesIO
+from PIL import Image
+import base64
+import torch
+
+
+
+def test_yolo(url):
+    model_path = "/home/allen/Dataset/tyre_damaged.pt"
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path)  # custom model
+    model.conf = 0.7
+    image = url
+    #image = Image.open(urllib.request.urlopen(url))
+    results = model(image, augment=True, size=640)
+    num_of_damages_detected = len(results.xyxy[0])
+    if num_of_damages_detected >= 1:
+        results.render()
+        for img in results.imgs:
+            buffered = BytesIO()
+            image_array = Image.fromarray(img)
+            image_array.save(buffered, format="JPEG")
+            image_to_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+            image_64_decode = base64.b64decode(image_to_base64)
+            image_result = open('damaged_tyre_decoder.jpeg',
+                                'wb')  # create a writable image and write the decoding result
+            image_result.write(image_64_decode)
+        return {'Response': image_to_base64}
+    return {'The tyre is OK'}
+
+
+
+
+
+
+url = 'https://raw.githubusercontent.com/shivajid/Yolo/master/data/eagle.jpg'
+img = '/home/allen/Dataset/Damage Segmentation2/test/IMG_0495.JPG'
+img2 = '/home/allen/Dataset/Tire-pavement-copy.jpg'
+print(test_yolo(url=img))
